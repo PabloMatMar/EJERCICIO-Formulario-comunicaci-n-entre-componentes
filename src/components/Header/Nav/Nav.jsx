405bab9cee9dacf1030369affd6e2c7025db751e
PabMatMar <pabmatmar@gmail.com>
2023-02-21 01:07:21 +0100

    add/ refactor message hello user!

--- src/components/Header/Nav/Nav.jsx
+++ src/components/Header/Nav/Nav.jsx
@@ -4,18 +4,19 @@ import { userContext } from '../../../context/userContext';
 
 class Nav extends Component {
   render() {
-    return <nav>
+    return <div><nav>
       <Link to="/">Home</Link>
       <Link to="/DataForm">Data</Link>
       <Link to="/Form">Form</Link>
-      <userContext.Consumer>
-        {({ logout, user }) => user ?
-          <span>Hola, {user.username}
+    </nav>
+    <userContext.Consumer>
+        {({ logout, user }) => user.username ?
+          <span>Hello, {user.username}!
           <button variant="contained" size="small" onClick={logout}>Logout</button></span>
           : ""
         }
       </userContext.Consumer>
-    </nav>;
+    </div>;
   }
 }
 
